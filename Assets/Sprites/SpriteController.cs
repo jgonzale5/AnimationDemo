c432632864f452c082765e8e9b021524bdb0c733
JohnathanWells <slaifir@hotmail.com>
2020-03-30 03:10:37 -0400

    Sprite Animation

---
+++ Assets/Sprites/SpriteController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteController : MonoBehaviour
+{
+    public Animator animator;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float input = Input.GetAxis("Horizontal");
+        animator.SetFloat("InputX", input);
+    }
+}
