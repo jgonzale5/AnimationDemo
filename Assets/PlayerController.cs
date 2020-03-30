a4b67bb99b49b21601177729ef022c047aa0dab2
JohnathanWells <slaifir@hotmail.com>
2020-03-30 00:27:19 -0400

    Initial commit

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Animator animator;
+    public CharacterController controller;
+    public float backwardSpeed;
+    public float forwardSpeed;
+    public float sideSpeed;
+    public KeyCode attackKey;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float inputY = Input.GetAxis("Vertical");
+        float inputX = Input.GetAxis("Horizontal");
+        Vector3 movement = Vector3.zero;
+
+        if (inputY > 0)
+        {
+            movement = new Vector3(inputX * sideSpeed, 0, inputY * forwardSpeed);
+            controller.Move(movement * Time.deltaTime);
+        }
+        else
+        {
+            movement = new Vector3(inputX * sideSpeed, 0, inputY * backwardSpeed);
+            controller.Move(movement * Time.deltaTime);
+        }
+        animator.SetFloat("SpeedY", inputY);
+        animator.SetFloat("SpeedX", inputX);
+
+        if (Input.GetKeyDown(attackKey))
+        {
+            animator.SetTrigger("Attack");
+        }
+    }
+}
---
+++ Assets/Scripts/ManagerScript.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+
+public class ManagerScript : MonoBehaviour
+{
+    public UnityEvent OnWin;
+    public UnityEvent OnLose;
+    public static ManagerScript Instance;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public void Win()
+    {
+        OnWin.Invoke();
+        SaveScript.saveFile.ScoreIncrease();
+    }
+
+    public void Lose()
+    {
+        OnLose.Invoke();
+    }
+
+    public void LoadSaveFile()
+    {
+        bool loadSuccess = SaveScript.LOAD();
+
+        if (loadSuccess)
+            SceneManager.LoadScene(SaveScript.saveFile.lastScene);
+    }
+
+    public void LoadScene(string withName)
+    {
+        SceneManager.LoadScene(withName);
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
